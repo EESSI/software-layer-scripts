7d621f10db92a15eea7c8e551db470b29659038c
Alan O'Cais <alan.ocais@cecam.org>
2026-01-30 11:36:28 +0100

    Make test init script listen to EESSI_SOFTWARE_SUBDIR_OVERRIDE

--- .github/workflows/scripts/test_init_scripts.sh
+++ .github/workflows/scripts/test_init_scripts.sh
@@ -11,6 +11,11 @@ if [ -z ${EXPECTED_EASYBUILD_VERSION} ]; then
   exit 1
 fi
 
+if [ -z ${EESSI_SOFTWARE_SUBDIR_OVERRIDE} ]; then
+  echo "\$EESSI_SOFTWARE_SUBDIR_OVERRIDE has to be set (e.g., x86_64/intel/haswell) so we can do well defined string comparison for the architecture."
+  exit 1
+fi
+
 # initialize assert framework
 if [ ! -d assert.sh ]; then
   echo "assert.sh not cloned."
@@ -50,7 +55,7 @@ for shell in ${SHELLS[@]}; do
     else
       MODULE_SECTIONS=($($shell -c "source init/lmod/$shell 2>/dev/null; module ov 2>&1 | grep -e '---'"))
     fi
-    PATTERN="/cvmfs/software\.eessi\.io/versions/$EESSI_VERSION/software/linux/x86_64/(intel/haswell|amd/zen3|intel/icelake)/modules/all"
+    PATTERN="/cvmfs/software\.eessi\.io/versions/$EESSI_VERSION/software/linux/$EESSI_SOFTWARE_SUBDIR_OVERRIDE/modules/all"
     assert_raises 'echo "${MODULE_SECTIONS[1]}" | grep -E "$PATTERN"'
     echo "${MODULE_SECTIONS[1]}" "$PATTERN"
 
@@ -75,7 +80,7 @@ for shell in ${SHELLS[@]}; do
       EASYBUILD_PATH=$($shell -c "source init/lmod/$shell 2>/dev/null; module load EasyBuild/${EXPECTED_EASYBUILD_VERSION}; which eb")
     fi
     # escape the dots in ${EASYBUILD_VERSION}
-    PATTERN="/cvmfs/software\.eessi\.io/versions/$EESSI_VERSION/software/linux/x86_64/(intel/haswell|amd/zen3|intel/icelake)/software/EasyBuild/${EXPECTED_EASYBUILD_VERSION//./\\.}/bin/eb"
+    PATTERN="/cvmfs/software\.eessi\.io/versions/$EESSI_VERSION/software/linux/$EESSI_SOFTWARE_SUBDIR_OVERRIDE/software/EasyBuild/${EXPECTED_EASYBUILD_VERSION//./\\.}/bin/eb"
     echo "$EASYBUILD_PATH" | grep -E "$PATTERN"
     assert_raises 'echo "$EASYBUILD_PATH" | grep -E "$PATTERN"'
     echo "$EASYBUILD_PATH" "$PATTERN"
