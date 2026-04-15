b7792e11c009bac2258ae0a228edf55a497a917d
Samuel Moors <sammoors@gmail.com>
2026-04-15 14:47:20 +0200

    fix

--- .github/workflows/scripts/test_init_scripts.sh
+++ .github/workflows/scripts/test_init_scripts.sh
@@ -173,8 +173,8 @@ for shell in ${SHELLS[@]}; do
         prompt="\$(echo '\['✘) $ "
         promptstr="\[✘ $ "
         updated_promptstr="{EESSI/${EESSI_VERSION}} \[✘ $ "
-        TEST_EESSI_PS1_UPDATE=$($shell -c "unset PS1 ; PS1=\"$prompt\" ; export EESSI_CVMFS_REPO=$PWD ; export LMOD_IGNORE_CACHE=1 ; . init/lmod/$shell.test >/dev/null ; echo \"\$PS1\"")
-        TEST_EESSI_PS1_REVERT=$($shell -c "unset PS1 ; PS1=\"$prompt\" ; export EESSI_CVMFS_REPO=$PWD ; export LMOD_IGNORE_CACHE=1 ; . init/lmod/$shell.test >/dev/null ; module purge; echo \"\$PS1\"")
+        TEST_EESSI_PS1_UPDATE=$($shell -c "unset PS1 ; PS1=\"$prompt\" ; . init/lmod/$shell.test >/dev/null ; echo \"\$PS1\"")
+        TEST_EESSI_PS1_REVERT=$($shell -c "unset PS1 ; PS1=\"$prompt\" ; . init/lmod/$shell.test >/dev/null ; module purge; echo \"\$PS1\"")
         assert 'echo "$TEST_EESSI_PS1_UPDATE"' "$updated_promptstr"
         assert 'echo "$TEST_EESSI_PS1_REVERT"' "$promptstr"
     fi
