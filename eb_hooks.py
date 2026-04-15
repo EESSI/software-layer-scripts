b91483d07d70ab87d689d704f19335fbfb990800
julianmorillo <julian.morillo@bsc.es>
2026-04-15 16:58:51 +0200

    Add specific paths

--- eb_hooks.py
+++ eb_hooks.py
@@ -1123,6 +1123,8 @@ def pre_configure_hook_dyninst(self, *args, **kwargs):
         binutils_lib_path = glob.glob(binutils_lib_path_glob_pattern)
         if len(binutils_lib_path) == 1:
             self.cfg.update('configopts', '-DLibIberty_ROOT_DIR=' + binutils_lib_path[0])
+            self.cfg.update('configopts', '-DLibIberty_INCLUDE_DIRS=' + os.path.join(binutils_lib_path[0], 'include'))
+            self.cfg.update('configopts', '-DLibIberty_LIBRARIES=' + binutils_lib_path[0])
         else:
             raise EasyBuildError("Failed to isolate path for binutils libraries using %s, got %s",
                                  binutils_lib_path_glob_pattern, binutils_lib_path)
