156b1dfc5bc01b0bedf553d9746cfe19a91f6510
Bob Dröge <b.e.droge@rug.nl>
2026-04-17 17:09:18 +0200

    fix typo

--- create_lmodsitepackage.py
+++ create_lmodsitepackage.py
@@ -197,7 +197,7 @@ local function eessi_cuda_and_libraries_enabled_load_hook(t)
                 cudaVersion = os.getenv("EESSI_CUDA_DRIVER_VERSION")
                 local cudaVersion_req = os.getenv("EESSICUDAVERSION")
                 -- Account for the fact that the script sourced above was designed to never return a non-zero exit
-                -- even if it failes to set EESSI_CUDA_DRIVER_VERSION
+                -- even if it fails to set EESSI_CUDA_DRIVER_VERSION
                 -- Essentially, we handle that case here by raising an error, which can be suppressed
                 if not cudaVersion or cudaVersion == "" then
                     local suppress_var = "EESSI_CUDA_DRIVER_VERSION_SUPPRESS_WARNING"
