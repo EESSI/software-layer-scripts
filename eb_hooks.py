35e9ab51c3f12da049bb57ddf118812b1102d3bb
ocaisa <alan.ocais@cecam.org>
2026-05-07 14:25:19 +0200

    Update eb_hooks.py
    Co-authored-by: Bob Dröge <b.e.droge@rug.nl>

--- eb_hooks.py
+++ eb_hooks.py
@@ -676,7 +676,7 @@ def parse_hook_qt6_libinput(ec, eprefix):
                             f"No libinput dependency found for {ec.name} {ec.version}, please update relevant Qt6 hook"
                         )
     else:
-        raise EasyBuildError("maturin-specific hook triggered for non-maturin easyconfig?!")
+        raise EasyBuildError("Qt6-specific hook triggered for non-maturin easyconfig?!")
 
 def parse_hook_maturin(ec, eprefix):
     """
