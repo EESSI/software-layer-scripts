23cc5700c56a107e2201e463bf1e15dda258332f
Caspar van Leeuwen <casparl@int5.local.snellius.surf.nl>
2026-05-19 09:59:26 +0200

    Add more verbosity to figure out what's wrong

--- bot/build.sh
+++ bot/build.sh
@@ -206,6 +206,20 @@ COMMON_ARGS+=("--mode" "run")
 # or on top of the EESSI compat layer (i.e. for dev.eessi.io)
 if [[ "${REPOSITORY_NAME}" == "dev.eessi.io" || -n "${EESSI_SITE_INSTALL_FORCE}" ]]; then
     COMMON_ARGS+=("--repository" "software.eessi.io,access=ro")
+
+fi
+
+# Override the compat layer if EESSI_CVMFS_COMPAT_REPO is defined. This allows using a different repo for the
+# compatibility layer compared to the EESSI_CVMFS_REPO (in which things will be installed)
+# (this is already done for dev.eessi.io through a customized SLURM script but that could probably be removed then)
+if [[ -n "${EESSI_CVMFS_COMPAT_REPO}" && -n "${EESSI_VERSION_OVERRIDE}" ]]; then
+    export EESSI_COMPAT_LAYER_DIR_OVERRIDE="${EESSI_CVMFS_COMPAT_REPO}/versions/${EESSI_VERSION_OVERRIDE}/compat/linux/$(uname -m)"
+    msg="Set EESSI_COMPAT_LAYER_DIR_OVERRIDE to $EESSI_COMPAT_LAYER_DIR_OVERRIDE since both EESSI_CVMFS_COMPAT_REPO"
+    msg ="(${EESSI_CVMFS_COMPAT_REPO}) and EESSI_VERSION_OVERRIDE (${EESSI_VERSION_OVERRIDE}) are defined"
+    echo "$msg"
+else
+    echo "EESSI_CVMFS_COMPAT_REPO: ${EESSI_CVMFS_COMPAT_REPO}"
+    echo "EESSI_VERSION_OVERRIDE: ${EESSI_VERSION_OVERRIDE}"
 fi
 
 # add $software_layer_dir and /dev as extra bind paths
