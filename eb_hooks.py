ae3ae48b9469b9c27fd08ecbebc25a2adc352c3c
Bob Dröge <b.e.droge@rug.nl>
2026-06-22 15:13:45 +0200

    allow 41 failing tests for PyTorch 2.9.1 on A64FX

--- eb_hooks.py
+++ eb_hooks.py
@@ -1711,11 +1711,15 @@ def pre_test_hook_increase_max_failed_tests_arm_PyTorch(self, *args, **kwargs):
     See https://github.com/EESSI/software-layer/issues/461
     """
     cpu_target = get_eessi_envvar('EESSI_SOFTWARE_SUBDIR')
-    if self.name == 'PyTorch' and self.version == '2.1.2':
-        if get_cpu_architecture() == AARCH64:
-            self.cfg['max_failed_tests'] = 10
-        if cpu_target in [CPU_TARGET_CASCADELAKE, CPU_TARGET_ICELAKE, CPU_TARGET_SAPPHIRE_RAPIDS]:
-            self.cfg['max_failed_tests'] = 4
+    if self.name == 'PyTorch':
+        if self.version == '2.1.2':
+            if get_cpu_architecture() == AARCH64:
+                self.cfg['max_failed_tests'] = 10
+            if cpu_target in [CPU_TARGET_CASCADELAKE, CPU_TARGET_ICELAKE, CPU_TARGET_SAPPHIRE_RAPIDS]:
+                self.cfg['max_failed_tests'] = 4
+        if self.version == '2.9.1':
+            if cpu_target == CPU_TARGET_A64FX:
+                self.cfg['max_failed_tests'] = 41
 
 
 def pre_test_hook_ignore_failing_tests_OpenBabel_a64fx(self, *args, **kwargs):
