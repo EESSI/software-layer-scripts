98264246cf2db6097ac5da878d025f64e53af77b
Alan O'Cais <alan.ocais@cecam.org>
2026-07-23 11:50:36 +0200

    Better error message

--- scripts/gpu_support/nvidia/install_cuda_and_libraries.sh
+++ scripts/gpu_support/nvidia/install_cuda_and_libraries.sh
@@ -295,7 +295,7 @@ if [ ${#PROCESSED_EASYSTACK_FILES[@]} -gt 0 ]; then
         echo_green "- ${EASYSTACK_FILE}"
     done
 else
-    fatal_error "Error: No matching CUDA easystack files were found in ${NVIDIA_EASYSTACKS_DIRECTORY}!"
+    fatal_error "Error: No matching CUDA easystack files were successfully found/processed in ${NVIDIA_EASYSTACKS_DIRECTORY}!"
 fi
 
 # Remove the temporary directory
